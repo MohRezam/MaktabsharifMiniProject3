b1f42dbdf8de9c97f69509bb1b9d02b9789f41d9
MohRezam <mkalhor81126@gmail.com>
2023-10-30 19:34:52 +0330

    changed html

--- manage_files.sh
+++ manage_files.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
+file="Icons.txt"
+out_dir="/home/digitcrom/Desktop/Miniproject/MaktabsharifMiniProject3/front/Icons"
+mkdir $out_dir
 
+while IFS= read -r line; do
+    cp "$line" "$out_dir"
+done < "$file"
 
+echo "successfully transferred"
